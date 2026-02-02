d208ea4f9abd210864608cf60c67d4eeee541f7d
tituscarl <titusranario@gmail.com>
2026-02-02 13:03:17 +0800

    modify log

--- simple-pubsub/main.go
+++ simple-pubsub/main.go
@@ -95,7 +95,7 @@ func main() {
 	http.HandleFunc("/publish", publishHandler)
 	http.HandleFunc("/subscribe", subscribeHandler)
 
-	log.Println("Starting server on :8080")
+	log.Println("Starting pubsub server on :8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
